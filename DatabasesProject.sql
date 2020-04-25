d78d8e3054ddf151fca2f9f0ba50aa2090691ab3
Erick Martinez <emartinez4@ggc.edu>
2020-04-24 20:56:17 -0400

    Added the starting script file.

---
+++ DatabasesProject.sql
@@ -0,0 +1,30 @@
+USE mydb;
+
+CREATE TABLE Restaurant(
+RestaurantName varchar(50) Primary Key,
+Avg_Rating int,
+Location varchar(45),
+Phone_Number INT(10),
+Specialty varchar(25),
+Category varchar(25),
+Days_Of_Operation varchar(25),
+Hours_Of_Operation varchar(25),
+Monetary_Tier varchar(25),
+Vegetarian_Option varchar(3),
+Self_Serve varchar(3),
+Vegan_Option varchar(3),
+Reservation_Required varchar(3),
+Restaurant_Chain varchar(3),
+Capacity INT,
+Parking_Capacity INT,
+Website varchar(120)
+);
+
+INSERT INTO Restaurant(RestaurantName, Avg_Rating, Location, Phone_Number, Specialty, Category, Days_Of_Operation, Hours_Of_Operation, Monetary_Tier, Vegetarian_Option, Self_Serve, Vegan_Option, Reservation_Required, Restaurant_Chain, Capacity, Parking_Capacity, Website)
+VALUES ("Fuckoffee", 5, "Atlanta, Georgia", 7709338393, "Coffee", "Barista", "Monday-Sunday", "7:00 am - 9:00 pm", "Low", "No", "No", "No", "No", "Yes", 20, 15, "https://FuckCofee.com");
+
+INSERT INTO Restaurant(RestaurantName, Avg_Rating, Location, Phone_Number, Specialty, Category, Days_Of_Operation, Hours_Of_Operation, Monetary_Tier, Vegetarian_Option, Self_Serve, Vegan_Option, Reservation_Required, Restaurant_Chain, Capacity, Parking_Capacity, Website)
+VALUES ("Baguetteaboutit", 5, "New York, New York", 6768338393, "Baguette", "Italian", "Monday-Sunday", "11:00 am - 10:00 pm", "High", "Yes", "No", "Yes", "Yes", "Yes", 20, 15, "https://FuckCofee.com");
+
+
+
